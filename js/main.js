d84fe260bd0173ac59dc466d59c6cbb5a1bb4710
Karol Skolasiński <kskolasinski@man.poznan.pl>
2026-07-06 17:03:47 +0200

    fix closing menu

--- js/main.js
+++ js/main.js
@@ -2,3 +2,18 @@ import "./comnponents/header.js";
 import "./comnponents/footer.js";
 import "./comnponents/benefits.js";
 import "./comnponents/review.js";
+
+document.addEventListener("DOMContentLoaded", () => {
+  const hamburgerCheckbox = document.getElementById("hamburger");
+  const menuList = document.getElementById("menu-list");
+
+  if (hamburgerCheckbox && menuList) {
+    const menuItems = menuList.querySelectorAll("a");
+
+    menuItems.forEach((item) => {
+      item.addEventListener("click", () => {
+        hamburgerCheckbox.checked = false;
+      });
+    });
+  }
+});
