2b9ea1fe04e5cfd5ecf4235d99c777be3a54cda0
Karol Skolasiński <kskolasinski@man.poznan.pl>
2026-07-12 12:21:32 +0200

    menu

--- js/comnponents/header.js
+++ js/comnponents/header.js
@@ -18,16 +18,26 @@ class Header extends HTMLElement {
             >
             </x-logo>
 
-            <button class="button hidden! xs:block!">
+            <button class="button hidden! lg:flex! gap-2">
               Umów wizytę
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                class="w-5 h-5 fill-current"
+                viewBox="0 -960 960 960"
+              >
+                <path
+                  d="M200-120q-33 0-56.5-23.5T120-200v-560q0-33 23.5-56.5T200-840h40v-80h80v80h320v-80h80v80h40q33 0 56.5 23.5T840-760v560q0 33-23.5 56.5T760-120H200Zm0-80h560v-400H200v400Zm0-480h560v-80H200v80Zm0 0v400-400Z"
+                />
+              </svg>
             </button>
           </div>
 
           <nav class="hidden md:flex items-center gap-6">
-            <a href="${prefix}about" class="hover:text-primary-400">Oferta</a>
+            <a href="${prefix}about" class="hover:text-primary-400">O nas</a>
             <a href="${prefix}services" class="hover:text-primary-400">Kadra</a>
             <a href="${prefix}reviews" class="hover:text-primary-400">Cennik</a>
             <a href="blog.html" class="hover:text-primary-400">Blog</a>
+            <a href="${prefix}contact" class="hover:text-primary-400">Kontakt</a>
           </nav>
 
           <nav class="md:hidden group">
@@ -58,7 +68,7 @@ class Header extends HTMLElement {
                   href="#"
                   class="block w-full py-5 px-6 transition-colors duration-150 active:bg-primary-500 active:text-white select-none"
                 >
-                  Oferta
+                  O nas
                 </a>
               </li>
 
@@ -87,6 +97,21 @@ class Header extends HTMLElement {
                 >
                   Blog
                 </a>
+              </li>
+
+              <li class="text-center pt-10">
+                <button class="button flex gap-2">
+                  Umów wizytę
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    class="w-5 h-5 fill-current"
+                    viewBox="0 -960 960 960"
+                  >
+                    <path
+                      d="M200-120q-33 0-56.5-23.5T120-200v-560q0-33 23.5-56.5T200-840h40v-80h80v80h320v-80h80v80h40q33 0 56.5 23.5T840-760v560q0 33-23.5 56.5T760-120H200Zm0-80h560v-400H200v400Zm0-480h560v-80H200v80Zm0 0v400-400Z"
+                    />
+                  </svg>
+                </button>
               </li>
 
               <li class="mt-auto w-full p-6 flex flex-col gap-5 bg-gray-50/50 border-t border-gray-100">
