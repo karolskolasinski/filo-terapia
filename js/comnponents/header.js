65b9d04023462d495f95068b29a166fe5eb04d4e
Karol Skolasiński <kskolasinski@man.poznan.pl>
2026-07-12 13:36:46 +0200

    adjust header, blog

--- js/comnponents/header.js
+++ js/comnponents/header.js
@@ -20,15 +20,6 @@ class Header extends HTMLElement {
 
             <button class="button hidden! lg:flex! gap-2">
               Umów wizytę
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                class="w-5 h-5 fill-current"
-                viewBox="0 -960 960 960"
-              >
-                <path
-                  d="M200-120q-33 0-56.5-23.5T120-200v-560q0-33 23.5-56.5T200-840h40v-80h80v80h320v-80h80v80h40q33 0 56.5 23.5T840-760v560q0 33-23.5 56.5T760-120H200Zm0-80h560v-400H200v400Zm0-480h560v-80H200v80Zm0 0v400-400Z"
-                />
-              </svg>
             </button>
           </div>
 
